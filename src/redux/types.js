6ac7c5faab6a715fad78d8a560580c4196fa7a31
Alexander Vitchinov <vitalex92@yandex.ru>
2021-11-14 16:10:27 +0300

    quickfix/remove unused types

--- src/redux/types.js
+++ src/redux/types.js
@@ -1,4 +1,2 @@
 export const FETCH_DATA = 'CURRENCY/FETCH_DATA';
 export const USER_LANG = 'LANG/USER_LANG';
-export const SHOW_LOADER = 'APP/SHOW_LOADER';
-export const HIDE_LOADER = 'APP/HIDE_LOADER';
